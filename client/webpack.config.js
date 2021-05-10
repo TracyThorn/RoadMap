15685ef9f1a341aad7f06b94fa9bdb01de989dbb
Tracy Thorn <ardiosomega@gmail.com>
2021-05-10 12:22:21 -0500

    started adding webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,16 @@
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode: mode,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ]
+  }
+}
