81d5c7454ff7a362dfc4bee41c542d1bdd54936e
Tracy Thorn <tracy@Tracys-MBP.home>
2021-05-17 20:57:38 -0500

    adding bootstrap and sass

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from "react-dom"
 import Home from "./pages/Home"
+import "../../scss/custom.scss"
 
 function App() {
   return (
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,6 +11,27 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
+      }
     ]
   }
 }
