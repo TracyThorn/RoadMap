9ed422a0dade8e3a9fa233d0c18b680db27a706e
Tracy Thorn <tracy@Tracys-MBP.home>
2021-06-28 13:10:47 -0500

    method testing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -17,29 +17,51 @@ const tempArr = [{
   onlineStatus: true
 }]
 
+const initialState = {
+  hiteBtn: false
+};
+
 class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      ...initialState
+    }
     this.fetch = this.fetch.bind(this);
   }
   
   fetch() {
     this.props.fetchPosts();
+    this.exampleMethod_updatesState();
   }
+
+  exampleMethod_updatesState() {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  }
+
+  exampleMethod_returnsAValue(number) {
+    return number + 1;
+  }
+
   render() {
     const { posts } = this.props;
-
+    const { hideBtn } = this.state;
     const configButton = {
       buttonText: 'Get Posts',
       emitEvent: this.fetch
     }
     return (
-      <div>
+      <div data-test="appComponent">
         <Header />
         <section className="main">
           <Headline header="Posts" desc="Click the button to render posts!" tempArr={tempArr} />
-          <SharedButton {...configButton} />
+          {!hideBtn &&
+            <SharedButton {...configButton} />
+          }
           {posts.length > 0 &&
             <div>
               {posts.map((post, index) => {
